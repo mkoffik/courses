4f18694a4bd8997a67d7d51f0ddb006ee3d7b2b8
mkoffik <michel_dove@outlook.fr>
2026-02-23 02:35:25 +0100

    jira-XX4 : modelisation des classes commandes et facture

--- src/main/java/com/api/courses/model/Commande.java
+++ src/main/java/com/api/courses/model/Commande.java
@@ -5,9 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Commande {
+    private String IdCommande;
+    private String IdClient;
+    private List<Article> article;
+    private String commentaire;
 }
--- src/main/java/com/api/courses/model/Facture.java
+++ src/main/java/com/api/courses/model/Facture.java
@@ -1,13 +1,24 @@
 package com.api.courses.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Facture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String idFacture;
+    private String idClient;
+    private List<Commande> commande;
+    private String commentaire;
 }
