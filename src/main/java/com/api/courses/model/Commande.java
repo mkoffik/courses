f34a3cfeca56ad2f66c2e9a89bcad3637de0b963
mkoffik <michel_dove@outlook.fr>
2026-02-23 02:01:28 +0100

    Jira-XX3 : Ajout des annotations lombok pour eviter le creation manuelle des getter et setter

--- src/main/java/com/api/courses/model/Commande.java
+++ src/main/java/com/api/courses/model/Commande.java
@@ -1,4 +1,13 @@
 package com.api.courses.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Commande {
 }
